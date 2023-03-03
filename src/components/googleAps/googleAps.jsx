aa4d14490b4f5d49d93ae80aeb9ebd0a8bb7ad3b
Khurshid Uzakov <xurshiduzoqov1@gmail.com>
2023-03-03 22:06:10 +0500

    Changing export Google Apps Data for optimizing

--- src/components/googleAps/googleAps.jsx
+++ src/components/googleAps/googleAps.jsx
@@ -1,8 +1,75 @@
 import React from "react";
-import { googleApsData } from "../../constants";
 import IconButton from "@material-ui/core/IconButton";
 import { Link } from "react-router-dom";
 import "./googleAps.css";
+
+import googleIcon from "/google_icon.png";
+import googleCalendar from "/google_calendar.png";
+import googleDrive from "/google_drive.png";
+import googleMeet from "/google_meet.png";
+import googleNews from "/google_news.png";
+import googleTranslate from "/google_translate.png";
+import googleWallet from "/google_wallet.png";
+import googleStore from "/play-store.png";
+
+const googleApsData = [
+  {
+    img: googleIcon,
+    title: "Search",
+    author: "author",
+    id: "1",
+    link: "https://www.google.com/",
+  },
+  {
+    img: googleCalendar,
+    title: "Calendar",
+    author: "author",
+    id: "2",
+    link: "https://calendar.google.com/calendar/",
+  },
+  {
+    img: googleDrive,
+    title: "Drive",
+    author: "author",
+    id: "3",
+    link: "https://drive.google.com/drive/",
+  },
+  {
+    img: googleMeet,
+    title: "Meet",
+    author: "author",
+    id: "4",
+    link: "https://meet.google.com/",
+  },
+  {
+    img: googleNews,
+    title: "News",
+    author: "author",
+    id: "5",
+    link: "https://news.google.com/",
+  },
+  {
+    img: googleTranslate,
+    title: "Translate",
+    author: "author",
+    id: "6",
+    link: "https://translate.google.co.uz/",
+  },
+  {
+    img: googleWallet,
+    title: "Shopping",
+    author: "author",
+    id: "7",
+    link: "https://www.google.co.uz/shopping",
+  },
+  {
+    img: googleStore,
+    title: "Play",
+    author: "author",
+    id: "8",
+    link: "https://play.google.com/store",
+  },
+];
 
 const GoogleAps = () => {
   return (
